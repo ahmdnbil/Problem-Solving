fa675d920cf3a5a8f14f4f9d889c1d66765f80e0
ahmdnbil <batmoon555@gmail.com>
2022-09-14 18:12:29 +0200

    commit

--- C/HackerRank/Calculate_the_Nth_term/lab.c
+++ C/HackerRank/Calculate_the_Nth_term/lab.c
@@ -6,17 +6,15 @@
 
 int find_nth_term(int n, int a, int b, int c) {
 
-    if(n==c)
-    return c;
-    else if(n==a)
+    if(n==1)
     return a;
-    else if(n==b)
+    else if(n==2)
     return b;
-    else if(n==0)
-    return 0;
+    else if(n==3)
+    return c;
     else
     {
-        return find_nth_term(n-1, a,b,c);
+        return find_nth_term(n-1, a,b,c)+find_nth_term(n-2, a,b,c)+find_nth_term(n-3, a,b,c);
     }
     
 }
