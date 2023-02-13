477fd28279272230f4172aacc0555cfa1858c81b
ahmdnbil <batmoon555@gmail.com>
2023-02-13 15:39:54 +0200

    sol to prob no.19 in leet

---
+++ C/leetcode/19-Remove_Nth_Node_From_End_of_List/lab.c
@@ -0,0 +1,39 @@
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+#define NULL 0
+typedef struct ListNode List; 
+
+List* removeNthFromEnd(List* head, int n)
+{
+    List* counter=head;
+    int count=0,flag=0;
+    if (head == NULL) return NULL;
+    count++;
+    while(counter->next != NULL)
+    {
+        count++;
+        counter=counter->next;
+    }
+    counter=head;
+
+    if(count == 1) return NULL;
+    while(counter->next !=NULL)
+    {
+        // count-- ;
+        if (count == n) return head->next;
+        else if(count == n+1) 
+        {
+            counter->next=counter->next->next;
+            break;
+        }
+        else
+        {
+            counter=counter->next;
+            count--;
+        }
+    }
+    return head;
+}
