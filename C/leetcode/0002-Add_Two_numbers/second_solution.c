30ae4c524628b57451616677cc3f76a6ebd087b8
ahmdnbil <batmoon555@gmail.com>
2023-02-22 02:10:11 +0200

    sol to prob no.2 in leetcode

--- C/leetcode/0002-Add_Two_numbers/second_solution.c
+++ C/leetcode/0002-Add_Two_numbers/second_solution.c
@@ -1,51 +1,78 @@
-
-
-#define NULL 0
-struct ListNode
-{
+//accepted in leetcode 
+#define NULL ((void*)0)
+struct ListNode {
     int val;
     struct ListNode *next;
 };
 
+
 typedef struct ListNode List;
-
-
-void addNode(int val,List* head)
+List head;
+int listLength =0;
+void addNode(int val)
 {
-    List* last=&head;
-    List* new=(List*)malloc(sizeof(List));
-    new->val=val;
-    new->next=NULL;
-    while(last->next != NULL)
+    if(listLength == 0)
     {
-        last=last->next;
+        head.val=val;
+        head.next=NULL;
     }
-    last->next=new;
+    else
+    {
+        List*last=&head;
+        List*new=(List*)malloc(sizeof(List));
+        new->val=val;
+        new->next=NULL;
+        while(last->next != NULL)
+        {
+            last=last->next;
+        }
+        last->next=new;
+    }
+    listLength++;
 }
-List* addTwoNumbers(List* l1, List* l2)
-{
-    int sum=0,rem=0,length=0,flag=0;
-    List *head=(List*)malloc(sizeof(List));
+List* addTwoNumbers(List* l1, List* l2){
+    listLength=0;
+    if(!l1 && !l2) return l1;
+    int nodeVal;
+    int rem=0;
     while(l1 && l2)
     {
-        flag=1;
-        sum=((l1->val)+(l2->val)+rem)%10;
-        rem=sum/10;
+        nodeVal=(l1->val + l2->val) + rem;
+        if(nodeVal >9)
+        {
+            rem=nodeVal/10;
+            nodeVal=nodeVal%10;
+        }
+        else rem=0;
+        addNode(nodeVal);
         l1=l1->next;
         l2=l2->next;
-        if (length==0)
-        {
-            head->val=sum;
-            head->next=NULL;
-            length++;
-        }
-        else
-        {
-            addNode(sum,head);
-        }
+        
     }
-    
-
-    if(flag==0)head=NULL;
-    return head;
+    while(l1)
+    {
+        nodeVal=l1->val + rem;
+        if(nodeVal>9)
+        {
+            rem=nodeVal/10;
+            nodeVal%=10;
+        }
+        else rem=0;
+        addNode(nodeVal);
+        l1=l1->next;
+    }
+    while(l2)
+    {
+        nodeVal=l2->val + rem;
+        if(nodeVal>9)
+        {
+            rem=nodeVal/10;
+            nodeVal %=10;
+        }
+        else rem=0;
+        addNode(nodeVal);
+        l2=l2->next;
+    }
+    if(rem) addNode(rem);
+    return &head;
 }
