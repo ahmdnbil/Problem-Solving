c6c1e6b75ca6c2c189f2571687f35851347f1591
ahmdnbil <batmoon555@gmail.com>
2023-03-11 00:04:18 +0200

    sol to prob no.1721

---
+++ C/leetcode/1721-Swapping_Nodes_in_a_Linked_List/lab.c
@@ -0,0 +1,50 @@
+#define NULL ((void*)0)
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+typedef struct ListNode List;
+List* swapNodes(List* head, int k){
+    //special case
+    if(!head || !head->next) return head;
+    
+    List*ptr=head,*ptr2=NULL;
+    int second,first,temp;
+    int count=0;
+    
+    //counting linkedList
+    while(ptr)
+    {
+        count++;
+        ptr=ptr->next;
+    }
+    
+    //check if the elemnet at the mid and count is odd
+    if(count%2 !=0 && k==(count/2)+1 ) return head;
+    
+    count--;//number of jumps
+    ptr=head;
+    first=count-k+1;
+    second=k-1;
+    if((count+1)%2==0 && k>((count+1)/2) || (count+1)%2!=0 && k>((count+1)/2)+1 )
+    {
+        temp=first;
+        first=second;
+        second=temp;
+    }
+    if(!first) ptr2=head;
+    while(count !=0)
+    {
+        
+        if(count==first)ptr2=ptr;
+        else if (count==second)break;
+        count--;
+        ptr=ptr->next;
+    }
+    first=ptr2->val;
+    ptr2->val=ptr->val;
+    ptr->val=first;
+
+    return head;
+}
